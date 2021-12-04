640f3cc809be86459f7fee8fd09694e602af62ea
imilisav <mili2970@mylaurier.ca>
2021-12-04 03:06:58 -0500

    Add DB logging and Graph Functionality

---
+++ app/src/main/java/com/cp470/healthyhawk/Weight_Database_Helper.java
@@ -0,0 +1,46 @@
+package com.cp470.healthyhawk;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class Weight_Database_Helper extends SQLiteOpenHelper {
+    private static final String CLASS_NAME = "WeightDatabaseHelper";
+
+    private static final String DATABASE_NAME = "HealthyHawk.db";
+    private static final int VERSION_NUM = 2;
+
+    public static final String KEY_ID = "_id";
+    public static final String KEY_DATE_WEIGHT_LOGGED = "date_logged";
+    public static final String KEY_WEIGHT = "weight";
+    public static final String TABLE_NAME = "weight_log";
+
+    public Weight_Database_Helper(Context ctx) {
+        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i(CLASS_NAME, "Calling onCreate");
+        db.execSQL("CREATE TABLE "
+                + TABLE_NAME + "("
+                + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + KEY_DATE_WEIGHT_LOGGED + " DATE NOT NULL, "
+                + KEY_WEIGHT + " FLOAT NOT NULL);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer) {
+        Log.i(CLASS_NAME, "Calling onUpgrade, oldVersion=" + oldVer + " newVersion=" + newVer);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVer, int newVer) {
+        Log.i(CLASS_NAME, "Calling onUpgrade, oldVersion=" + oldVer + " newVersion=" + newVer);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/cp470/healthyhawk/Weight_Log.java
+++ app/src/main/java/com/cp470/healthyhawk/Weight_Log.java
@@ -3,9 +3,12 @@ package com.cp470.healthyhawk;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +18,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Weight_Log extends AppCompatActivity {
+
+    // Database variables
+    Weight_Database_Helper weightDBHelper;
+    SQLiteDatabase db;
+    Cursor cursor;
 
     // SharedPreferences variables
     String preference_file_name;
@@ -32,9 +47,20 @@ public class Weight_Log extends AppCompatActivity {
     TextView currentWeightText;
     TextView goalWeightText;
 
+    // GraphView specific variables
+    final int NUM_MAX_GRAPH_DATA_POINTS = 30;
+    DataPoint[] weightDataPoints;
+    DataPoint[] goalDataPoints;
+    LineGraphSeries<DataPoint> weightSeries;
+    LineGraphSeries<DataPoint> goalWeightSeries;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight_log);
+
+        // Getting access to database
+        weightDBHelper = new Weight_Database_Helper(this);
+        db = weightDBHelper.getWritableDatabase();
 
         // Setting up the SharedPreferences
         preference_file_name = getString(R.string.preference_file_name);
@@ -52,11 +78,12 @@ public class Weight_Log extends AppCompatActivity {
         goalWeightText = findViewById(R.id.textWeightLogGoal);
         goalWeightText.setText(goalWeightString);
 
-        // Populating the graph, default shows week view
+        // Populating the graph, default shows recent seven data points
         weightProgressChart = findViewById(R.id.weightProgressChart);
+        setupGraphView();
 
-
-        // Setting onClickListeners
+        // Initializing onClickListeners
+        // Log Weight Button
         setWeightButton = findViewById(R.id.logWeightButton);
         setWeightButton.setOnClickListener(view -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(Weight_Log.this);
@@ -79,6 +106,7 @@ public class Weight_Log extends AppCompatActivity {
             builder.show();
         });
 
+        // Set New Goal Button
         setGoalWeightButton = findViewById(R.id.logGoalWeightButton);
         setGoalWeightButton.setOnClickListener(view -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(Weight_Log.this);
@@ -102,29 +130,127 @@ public class Weight_Log extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
+    }
+
+    private void setupGraphView() {
+        // Set up graph title
+        weightProgressChart.setTitle(getString(R.string.WeightChartTitle));
+
+        // Graph legend set up
+        LegendRenderer lr = weightProgressChart.getLegendRenderer();
+        lr.setVisible(true);
+        lr.setAlign(LegendRenderer.LegendAlign.TOP);
+        lr.setBackgroundColor(Color.TRANSPARENT);
+
+        // Graph grid set up
+        GridLabelRenderer glr = weightProgressChart.getGridLabelRenderer();
+        glr.setGridStyle(GridLabelRenderer.GridStyle.HORIZONTAL);
+        glr.setHorizontalLabelsVisible(false);
+        glr.setPadding(32);
+
+        // Create two series of data: users weight from db, and SharedPreferences goal weight
+        // Get db related variables set up
+        cursor = db.rawQuery("SELECT * FROM " + Weight_Database_Helper.TABLE_NAME + ";", null);
+        int columnIndex = cursor.getColumnIndex(Weight_Database_Helper.KEY_WEIGHT);
+        cursor.moveToFirst();
+
+        // Get the data point arrays for each series set up
+        int numRows = cursor.getCount();
+        weightDataPoints = new DataPoint[numRows];
+        goalDataPoints = new DataPoint[numRows];
+
+        // Adding every weight logged on graph, graphview allows for users to zoom in/out
+        double goalWeight = Double.parseDouble(mPrefs.getString(key_weight_goal, "0.0"));
+        int i = 0;
+        while (!cursor.isAfterLast()) {
+            goalDataPoints[i] = new DataPoint(i, goalWeight);
+            weightDataPoints[i] = new DataPoint(i, cursor.getFloat(columnIndex));
+            ++i;
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+        // Add the respective data points to the graph
+        weightSeries = new LineGraphSeries<>(weightDataPoints);
+        weightSeries.setTitle("Current Weight");
+        weightSeries.setColor(Color.rgb(51, 0, 114)); //purple_a
+        weightProgressChart.addSeries(weightSeries);
+
+        goalWeightSeries = new LineGraphSeries<>(goalDataPoints);
+        goalWeightSeries.setTitle("Goal");
+        goalWeightSeries.setColor(Color.GREEN);
+        weightProgressChart.addSeries(goalWeightSeries);
+    }
+
+    private void updateGraphView(String weight) {
+        // goalWeight data point added to extend the goal line in graph
+        double goalWeight = Double.parseDouble(mPrefs.getString(key_weight_goal, "0.0"));
+        double weightInput = Double.parseDouble(weight);
+
+        DataPoint newWeightDataPoint = new DataPoint(weightDataPoints.length, weightInput);
+        DataPoint newGoalDataPoint = new DataPoint(goalDataPoints.length, goalWeight);
+
+        weightSeries.appendData(newWeightDataPoint, false, NUM_MAX_GRAPH_DATA_POINTS);
+        goalWeightSeries.appendData(newGoalDataPoint, false, NUM_MAX_GRAPH_DATA_POINTS);
+
+        // Let graph view know data has updated, to re-render
+        weightProgressChart.onDataChanged(false, false);
+    }
+
+    private void updateGraphViewGoalLineOnly() {
+        double updatedGoalWeight = Double.parseDouble(mPrefs.getString(key_weight_goal, "0.0"));
+        for (int i = 0; i < goalDataPoints.length; i++) {
+            goalDataPoints[i] = new DataPoint(i, updatedGoalWeight);
+        }
+        goalWeightSeries.resetData(goalDataPoints); // calls onDataChanged internally
+    }
+
     private void updateCurrentWeight(String weight) {
+        // Update the TextView
         String weightString = weight + " " + mPrefs.getString(key_weight_unit, "");
         currentWeightText.setText(weightString);
 
+        // Update the SharedPreferences
         SharedPreferences.Editor mEditor = mPrefs.edit();
         mEditor.putString(key_weight, weight);
-
         if (!mEditor.commit()) {
             String commit_failed = getString(R.string.commit_preferences_failed);
             Toast.makeText(this, commit_failed, Toast.LENGTH_SHORT).show();
+            return;
         }
+
+        // Add logged weight into db
+        SimpleDateFormat dateWeightLoggedFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date weightDateLogged = new Date();
+        ContentValues weightLoggedValues = new ContentValues();
+        weightLoggedValues.put(Weight_Database_Helper.KEY_DATE_WEIGHT_LOGGED, dateWeightLoggedFormat.format(weightDateLogged));
+        weightLoggedValues.put(Weight_Database_Helper.KEY_WEIGHT, Float.parseFloat(weight));
+        db.insert(Weight_Database_Helper.TABLE_NAME, null, weightLoggedValues);
+
+        // Update the GraphView with new weight
+        updateGraphView(weight);
     }
 
     private void updateGoalWeight(String goalWeight) {
+        // Update the TextView
         String goalWeightString = goalWeight + " " + mPrefs.getString(key_weight_unit, "");
         goalWeightText.setText(goalWeightString);
 
+        // Update the SharedPreferences
         SharedPreferences.Editor mEditor = mPrefs.edit();
         mEditor.putString(key_weight_goal, goalWeight);
-
         if (!mEditor.commit()) {
             String commit_failed = getString(R.string.commit_preferences_failed);
             Toast.makeText(this, commit_failed, Toast.LENGTH_SHORT).show();
+            return;
         }
+
+        // Update the goal line on the GraphView
+        updateGraphViewGoalLineOnly();
     }
 }
